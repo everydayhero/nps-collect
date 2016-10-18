a705ec895b47d91c50db7a40ce951f802d1020d2
Justin Marrington <justin@marrington.net>
2016-10-18 16:56:46 +1000

    Added image and icon assets

---
+++ source/images.js
@@ -0,0 +1,21 @@
+import clap from '../assets/clap.svg'
+import chat from '../assets/chat.svg'
+import logo from '../assets/logo.svg'
+
+export default {
+  clap: {
+    src: clap,
+    alt: 'Clap emoji',
+    className: 'icon-clap'
+  },
+  chat: {
+    src: chat,
+    alt: 'Chat emoji',
+    className: 'icon-chat'
+  },
+  logo: {
+    src: logo,
+    alt: 'Everydayhero Logo',
+    className: 'primary-logo'
+  }
+}
