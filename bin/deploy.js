112de86b0419184cbae04b4a93aaa5a8b047619a
Aaron Abrook <aabrook@gmail.com>
2016-11-11 11:51:24 +1000

    Assert required env vars are present

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,10 +1,23 @@
 const s3 = require('s3')
+const requiredEnvVars = require('./required-vars')
 
 const client = s3.createClient({
   maxAsyncS3: 5
 })
 
+const tokenPresent = (token) => !!process.env[token]
+const tokenNotPresent = (token) => !tokenPresent(token)
+
+const assertEnvVars = () =>
+  requiredEnvVars.filter(tokenNotPresent)
+
 const generate = () => {
+  const missingVars = assertEnvVars()
+  if (missingVars.length) {
+    console.log("Can't deploy as there are env vars missing", missingVars)
+    return
+  }
+
   console.log("Preparing to publish")
   const params = {
     localDir: 'dist',
@@ -18,6 +31,7 @@ const generate = () => {
   console.log('Publishing...')
   client.uploadDir(params)
     .on('error', (err) => {
+      console.error('Sync failed', err)
       throw new Error('Failed S3 Sync', err.stack)
     })
     .on('end', () => {
---
+++ bin/required-vars.js
@@ -0,0 +1,6 @@
+module.exports = [
+  'BUCKET',
+  'BUCKET_PREFIX',
+  'JEFFREY_TOKEN'
+]
+
