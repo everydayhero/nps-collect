d5aff8689036285105c27e83509ae0494c75871c
Aaron Abrook <aabrook@gmail.com>
2016-11-15 18:18:29 +1000

    Refactor the App into it's own component

--- source/client.js
+++ source/client.js
@@ -1,25 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Helmet from 'react-helmet'
-import images from './images'
-import { links, metatags } from './lib/header'
-import 'minimal.css'
+import App from './components/App'
 import { parse } from 'query-string'
 
-import NPSCollect from './components/NPSCollect'
-import NPSHeader from './components/NPSHeader'
-
 const mount = document.getElementById('mount')
+const query = parse(window.location.search)
 
-const pageDetails = parse(window.location.search)
-
-render(
-  <div>
-    <Helmet
-      title='everydayhero Feedback'
-      meta={metatags}
-      link={links}
-    />
-    <NPSHeader images={images} homepage='http://www.everydayhero.com' />
-    <NPSCollect images={images} {...pageDetails} />
-  </div>, mount)
+render(<App query={query} />, mount)
