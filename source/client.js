4edf3b9794d74180783349516ad4cc95c6dfe285
Aaron Abrook <aabrook@gmail.com>
2016-11-15 16:07:27 +1000

    Add header/favicon info

--- source/client.js
+++ source/client.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Helmet from 'react-helmet'
 import images from './images'
+import { links, metatags } from './lib/header'
 import 'minimal.css'
 import { parse } from 'query-string'
 
@@ -13,7 +15,11 @@ const pageDetails = parse(window.location.search)
 
 render(
   <div>
+    <Helmet
+      title='everydayhero Feedback'
+      meta={metatags}
+      link={links}
+    />
     <NPSHeader images={images} homepage='http://www.everydayhero.com' />
     <NPSCollect images={images} {...pageDetails} />
-  </div>
-, mount)
+  </div>, mount)
---
+++ source/lib/header.js
@@ -0,0 +1,5 @@
+export const metatags = [
+  { 'name': 'description', 'content': 'Make your voice, time, effort and money all count for the causes you love and see your impact on your Giving Footprint with everydayhero. Get started now!' }
+]
+
+export const links = [{ 'rel': 'icon', 'href': `/favicons/favicon.ico` }]
