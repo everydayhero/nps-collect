ebcec644795908bba42febfcb2d5ea333bf854a0
Brad Parker <hi@bradparker.com>
2016-11-16 16:47:17 +1000

    Output React Helmet elements during server render

--- source/server.js
+++ source/server.js
@@ -1,3 +1,4 @@
+import Helmet from 'react-helmet'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './components/App'
@@ -10,14 +11,18 @@ const renderStyles = styles => (
   styles.map(style => `<link rel="stylesheet" href=${style} />`).join('')
 )
 
-const renderDocument = ({ assets = [], content = '' }) => {
+const renderDocument = ({ assets = [], content = '', head }) => {
   const styles = assets.filter((asset) => asset.match(/\.css$/))
   const scripts = assets.filter((asset) => asset.match(/\.js$/))
 
   return (`
     <!doctype html>
-    <html>
+    <html ${head.htmlAttributes.toString()}>
       <head>
+        <meta charset="UTF-8" />
+        ${head.title.toString()}
+        ${head.meta.toString()}
+        ${head.link.toString()}
         ${renderStyles(styles)}
       </head>
       <body>
@@ -32,6 +37,7 @@ export default ({ assets }) => (route) => (
   Promise.resolve({
     result: renderDocument({
       content: renderToString(<App />),
+      head: Helmet.rewind(),
       assets
     })
   })
